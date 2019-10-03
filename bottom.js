be373dd814f87705ba1a9f580dc0b9c147808c15
zerobetest <samlettrois@gmail.com>
2019-10-03 23:13:50 +0900

    3.html, 4.html 수정 및 bottom.js 생성
    3.html에 내용 추가
    4.html에 내용 추가
    bottom.js를 생성하여 index~4.html의 bottom부분을 수정하였다.

---
+++ bottom.js
@@ -0,0 +1,8 @@
+const bottom = document.querySelector("#bottom"),
+    bottom_text = bottom.querySelector("p");
+
+const years = "2019";
+const months = "10";
+const days = "03";
+
+bottom_text.innerText = `TEST용 | 최종 수정 ${years} ${months} ${days}`;
